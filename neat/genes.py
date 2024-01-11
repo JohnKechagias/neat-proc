316bce1c4e8a0ec424691ddf267aed735439e205
John Kechagias <john.kechagias@stonebranch.com>
2024-01-11 19:01:11 +0200

    Update genes

--- neat/genes.py
+++ neat/genes.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 
+import math
 import copy
 import random
 from dataclasses import dataclass
-from enum import StrEnum, auto
+from enum import StrEnum, Enum, auto
 from inspect import get_annotations
-from typing import Callable
+from typing import Callable, Type
 
+from neat.utils import clamp
+from neat.parameters import GenomeParams
 from neat.activations import ActivationFuncs, get_activation_func
 from neat.aggregations import AggregationFuncs, get_aggregation_func
 from neat.types import LinkID, NodeID
@@ -19,13 +23,123 @@ class NodeType(StrEnum):
 
 
 @dataclass
-class NodeTraits:
+class Gene(ABC):
+    id: int
+
+    _excluded_attrs_from_mutation = ["id"]
+
+    def __repr__(self) -> str:
+        attrs_strings: list[str] = []
+
+        for attr in get_annotations(self.__class__).keys():
+            value = getattr(self, attr)
+            attrs_strings.append(f"{attr}={value}")
+
+        content = ", ".join(attrs_strings)
+        return f"{self.__class__.__name__}({content})"
+
+    @abstractmethod
+    def distance(self, other: Type[Gene]) -> float:
+        """Returns the distance between the genes. Distance 
+        measures how different the genes are."""
+
+    def crossover(self, other: Gene) -> Gene:
+        """Return a new Gene that is a combination of the two. It 
+        inherits values from both parents with equal probability."""
+        assert self.id == other.id
+
+        attrs = {}
+        for attr in get_annotations(self.__class__):
+            if random.random() < 0.5:
+                attrs[attr] = getattr(self, attr)
+            else:
+                attrs[attr] = getattr(other, attr)
+
+        return self.__class__(**attrs)
+
+    def mutate(self, params: GenomeParams):
+        for attr, attr_type in get_annotations(self.__class__).items():
+            if attr in self._excluded_attrs_from_mutation:
+                continue
+
+            if issubclass(attr_type, float):
+                self.mutate_float(attr, params)
+            if issubclass(attr_type, int):
+                self.mutate_int(attr, params)
+            if issubclass(attr_type, bool):
+                self.mutate_bool(attr, params)
+            if issubclass(attr_type, Enum):
+                self.mutate_enum(attr, params)
+
+    def mutate_float(self, attr: str, params: GenomeParams):
+        mutation_chance: float = getattr(params, f"{attr}_mutation_chance")
+        replace_chance: float = getattr(params, f"{attr}_replace_chance")
+
+        if random.random() < mutation_chance:
+            min_value: int = getattr(params, f"{attr}_min_value")
+            max_value: int = getattr(params, f"{attr}_max_value")
+
+            if random.random() < replace_chance:
+                init_mean: float = getattr(params, f"{attr}_init_mean")
+                init_stdev: float = getattr(params, f"{attr}_init_stdev")
+                value = random.gauss(init_mean, init_stdev)
+            else:
+                mutation_power: float = getattr(params, f"{attr}_mutation_power")
+                curr_value: int = getattr(self, attr)
+                change = random.gauss(0.0, mutation_power)
+                value = curr_value + change
+
+            setattr(self, attr, clamp(value, min_value, max_value))
+
+    def mutate_int(self, attr: str, params: GenomeParams):
+        mutation_chance: float = getattr(params, f"{attr}_mutation_chance")
+        replace_chance: float = getattr(params, f"{attr}_replace_chance")
+
+        if random.random() < mutation_chance:
+            min_value: int = getattr(params, f"{attr}_min_value")
+            max_value: int = getattr(params, f"{attr}_max_value")
+
+            if random.random() < replace_chance:
+                init_mean: float = getattr(params, f"{attr}_init_mean")
+                init_stdev: float = getattr(params, f"{attr}_init_stdev")
+                value = random.gauss(init_mean, init_stdev)
+            else:
+                mutation_power: float = getattr(params, f"{attr}_mutation_power")
+                curr_value: int = getattr(self, attr)
+                change = random.gauss(0.0, mutation_power)
+                value = curr_value + change
+
+            setattr(self, attr, math.ceil(clamp(value, min_value, max_value)))
+
+    def mutate_enum(self, attr: str, params: GenomeParams):
+        mutation_chance: float = getattr(params, f"{attr}_mutation_chance")
+
+        if random.random() < mutation_chance:
+            options: list[StrEnum] = getattr(params, f"{attr}_options")
+            value = random.choice(options)
+            setattr(self, attr, value)
+
+    def mutate_bool(self, attr: str, params: GenomeParams):
+        mutation_chance: float = getattr(params, f"{attr}_mutation_chance")
+
+        if random.random() < mutation_chance:
+            curr_value: bool = getattr(self, attr)
+            setattr(self, attr, not curr_value)
+
+    def copy(self) -> Gene:
+        return copy.copy(self)
+
+
+@dataclass
+class Node(Gene):
+    id: NodeID
+    node_type: NodeType
     bias: float
     response: float
     aggregator: AggregationFuncs
     activator: ActivationFuncs
 
-    def distance(self, other: NodeTraits) -> float:
+    def distance(self, other: Node) -> float:
         distance = abs(self.bias - other.bias)
         distance += abs(self.response - other.response)
 
@@ -37,43 +151,9 @@ class NodeTraits:
 
         return distance
 
-    def crossover(self, other: NodeTraits) -> NodeTraits:
-        attrs = {}
-        for attr in get_annotations(NodeTraits):
-            if random.random() < 0.5:
-                attrs[attr] = getattr(self, attr)
-            else:
-                attrs[attr] = getattr(other, attr)
-
-        return NodeTraits(**attrs)
-
-
-@dataclass
-class Node:
-    id: NodeID
-    traits: NodeTraits
-    node_type: NodeType
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(ID={self.id}, bias={self.traits.bias}, response={self.traits.response}, aggregator={self.traits.aggregator}, activator={self.traits.activator})"
-
-    def distance(self, other: Node) -> float:
-        return self.traits.distance(other.traits)
-
-    def crossover(self, other: Node) -> Node:
-        assert self.id == other.id
-        new_traits = self.traits.crossover(other.traits)
-        return Node(self.id, new_traits, self.node_type)
-
-    def add_value(self, value: float):
-        self.input.append(value)
-
-    def reset_value(self):
-        self.input = []
-
     def get_evaluator(self) -> Callable[[list[float]], float]:
-        aggregator = get_aggregation_func(self.traits.aggregator)
-        activator = get_activation_func(self.traits.activator)
+        aggregator = get_aggregation_func(self.aggregator)
+        activator = get_activation_func(self.activator)
 
         if self.node_type in (NodeType.INPUT, NodeType.OUTPUT):
 
@@ -85,27 +165,26 @@ class Node:
             def evaluate(input: list[float]) -> float:
                 aggregated_input = aggregator(input)
                 return activator(
-                    aggregated_input * self.traits.response + self.traits.bias
+                    aggregated_input * self.response + self.bias
                 )
 
         else:
-            raise NotImplementedError(
-                f"Activation of {self.node_type} is not currently implemented."
-            )
+            msg = f"Activation of {self.node_type} is not currently implemented."
+            raise NotImplementedError(msg)
 
         return evaluate
 
-    def copy(self) -> Node:
-        return copy.deepcopy(self)
-
 
 @dataclass
-class LinkTraits:
+class Link(Gene):
+    id: LinkID
+    in_node: NodeID
+    out_node: NodeID
     weight: float = 1
     enabled: bool = True
     frozen: bool = False
 
-    def distance(self, other: LinkTraits) -> float:
+    def distance(self, other: Link) -> float:
         distance = abs(self.weight - other.weight)
 
         if self.enabled != other.enabled:
@@ -116,64 +195,12 @@ class LinkTraits:
 
         return distance
 
-    def crossover(self, other: LinkTraits) -> LinkTraits:
-        attrs = {}
-        for attr in get_annotations(LinkTraits):
-            if random.random() < 0.5:
-                attrs[attr] = getattr(self, attr)
-            else:
-                attrs[attr] = getattr(other, attr)
-
-        return LinkTraits(**attrs)
-
-
-@dataclass
-class Link:
-    id: LinkID
-    in_node: NodeID
-    out_node: NodeID
-    traits: LinkTraits
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(ID={self.id}, ({self.in_node} -> {self.out_node}), weight={self.weight}, enabled={self.enabled})"
-
-    @property
-    def enabled(self) -> bool:
-        return self.traits.enabled
-
-    @property
-    def frozen(self) -> bool:
-        return self.traits.frozen
-
-    @property
-    def weight(self) -> float:
-        return self.traits.weight
-
     @property
     def simple_link(self) -> tuple[NodeID, NodeID]:
         return (self.in_node, self.out_node)
 
     def enable(self):
-        self.traits.enabled = True
+        self.enabled = True
 
     def disable(self):
-        self.traits.enabled = False
-
-    def reset_weight(self):
-        self.traits.weight = 1
-
-    def distance(self, other: Link) -> float:
-        return self.traits.distance(other.traits)
-
-    def crossover(self, other: Link) -> Link:
-        assert self.id == other.id
-        new_traits = self.traits.crossover(other.traits)
-        return Link(
-            self.id,
-            self.in_node,
-            self.out_node,
-            new_traits,
-        )
-
-    def copy(self) -> Link:
-        return copy.deepcopy(self)
+        self.enabled = False
