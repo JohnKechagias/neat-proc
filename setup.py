6f2226546d15bd1dee04474bbf2cbda1a16c3ce7
John Kechagias <kechagiasjohn@yahoo.gr>
2024-02-05 22:58:34 +0200

    Enhance setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import find_packages, setup
 
-setup(name="neat-proc", version="1.0", packages=find_packages())
+setup(
+    name="neat-proc",
+    version="1.0",
+    author="John Kechagias",
+    author_email="john.n.kechagias@gmail.com",
+    description="An implementation of the NEAT algorithm.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
